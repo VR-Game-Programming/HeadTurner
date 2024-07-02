821fc03f74413981ae623374c543801bc7cbeb6f
MarkymarkLee <94973921+MarkymarkLee@users.noreply.github.com>
2024-07-02 17:13:11 +0800

    update emg (#4)
    * add reading emgdata to unity
    
    * add arduino emg code
    
    * solve confilct
    
    * remove .vscode
    
    * update gitignore
    
    * fix merge
    
    * fix merge

--- EMG_Test/EMG_Test.ino
+++ EMG_Test/EMG_Test.ino
@@ -5,9 +5,9 @@ int emg_pins[4] = {A0, A1, A2, A3};
 unsigned long myTime;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("starting");
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("starting");
 }
 
 void loop() {
@@ -16,9 +16,9 @@ void loop() {
     myTime = millis();
     data += String(myTime) + ", ";
 
-  for(int i=0;i<pin_count;i++){
-    int result = analogRead(emg_pins[i]);
-    data += String(result) + ", ";
-  }
-  Serial.println(data);
+    for (int i = 0; i < pin_count; i++) {
+        int result = analogRead(emg_pins[i]);
+        data += String(result) + ", ";
+    }
+    Serial.println(data);
 }
